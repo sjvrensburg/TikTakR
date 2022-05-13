7999920c46beeb13211c1b4ce4ad050ef1169e77
sjvrensburg <sjansevanrensburg@gmail.com>
2022-05-13 12:39:34 +0200

    Updated the tests of the tiktak function.

--- tests/testthat/test_tiktak.R
+++ tests/testthat/test_tiktak.R
@@ -1,4 +1,4 @@
-test_that("tiktak function results appropriate answer",
+test_that("tiktak function results appropriate answer for NLOPT example problem",
     {
         # Example showing how to solve the problem
         # from the NLopt tutorial.
@@ -57,7 +57,64 @@ test_that("tiktak function results appropriate answer",
                         eval_g_ineq = g, opts = opts, a = a, b = b)
 
         # Test
-        expect_equal(res_1[[n]]$objective, eval_f0(solution.opt))
-        expect_equal(res_2[[n]]$objective, eval_f0(solution.opt))
-        expect_equal(res_3[[n]]$objective, eval_f0(solution.opt))
+        expect_equal(res_1$objective, eval_f0(solution.opt))
+        expect_equal(res_2$objective, eval_f0(solution.opt))
+        expect_equal(res_3$objective, eval_f0(solution.opt))
     })
+
+test_that("tiktak works for optimising the Rastrigin function.", {
+  # The Rastrigin is a challenging function to optimise since it has
+  # many many local minima.
+  rastrigin_fun <- function(x, A = 10) {
+    n <- length(x)
+    A*n + sum(x^2 - A * cos(2 * pi * x)) + 1
+  }
+  rastrigin_grad <- function(x, A = 10) {
+    # I'm being lazy and going to use numerical differentiation.
+    numDeriv::grad(rastrigin_fun, x, A = A)
+  }
+  
+  # Setup the optimisation... we want to have many short runs of the local search.
+  n <- 10; N <- 100; lb <- rep(-5.12, 2); ub <- rep(5.12, 2)
+  max_time <- 0.25  # a quarter of a second.
+  xtol_rel <- 1e-6
+  opts_sbplx <- list("maxtime" = max_time, "xtol_rel" = xtol_rel, "algorithm" = "NLOPT_LN_SBPLX", "print_level" = 3)
+  opts_lbfgs <- list("maxtime" = max_time, "xtol_rel" = xtol_rel, "algorithm" = "NLOPT_LD_LBFGS", "print_level" = 3)
+  
+  res_sbplx <- tiktak(eval_f = rastrigin_fun, n = n, lb = lb, ub = ub, N = N, opts = opts_sbplx, A = 10)
+  res_lbfgs <- tiktak(eval_f = rastrigin_fun, eval_grad_f = rastrigin_grad, n = n, lb = lb, ub = ub, N = N, opts = opts_lbfgs, A = 10)
+  
+  expect_equal(res_sbplx$solution, c(0, 0), label = "TikTak + Sbplx")
+  expect_equal(res_lbfgs$solution, c(0, 0), label = "TikTak + L-BFGS")
+})
+
+test_that("tiktak works for optimising the Rastrigin function when the starting values do not include the optimal solution.", {
+  # The Rastrigin is a challenging function to optimise since it has
+  # many many local minima.
+  rastrigin_fun <- function(x, A = 10) {
+    n <- length(x)
+    A*n + sum(x^2 - A * cos(2 * pi * x)) + 1
+  }
+  rastrigin_grad <- function(x, A = 10) {
+    # I'm being lazy and going to use numerical differentiation.
+    numDeriv::grad(rastrigin_fun, x, A = A)
+  }
+  
+  # Setup the optimisation... we want to have many short runs of the local search.
+  n <- 50; N <- 1000; lb <- rep(-5.12, 2); ub <- rep(5.12, 2)
+  max_time <- 2.4
+  xtol_rel <- 1e-8
+  opts_sbplx <- list("maxtime" = max_time, "xtol_rel" = xtol_rel, "algorithm" = "NLOPT_LN_SBPLX", "print_level" = 3)
+  opts_lbfgs <- list("maxtime" = max_time, "xtol_rel" = xtol_rel, "algorithm" = "NLOPT_LD_LBFGS", "print_level" = 3)
+  
+  init_res <- initialise(n + 1, N, lb, ub, rastrigin_fun, A = 10)
+  # Remove the optimal solution...
+  init_res$Parameters <- init_res$Parameters[-1, ]
+  init_res$Objective <- init_res$Objective[-1]
+  
+  res_sbplx <- tiktak(eval_f = rastrigin_fun, n = n, lb = lb, ub = ub, N = N, opts = opts_sbplx, init_res = init_res, A = 10)
+  res_lbfgs <- tiktak(eval_f = rastrigin_fun, eval_grad_f = rastrigin_grad, n = n, lb = lb, ub = ub, N = N, opts = opts_lbfgs, A = 10)
+  
+  expect_equal(res_sbplx$solution, c(0, 0), label = "TikTak + Sbplx (Expected to fail)")
+  expect_equal(res_lbfgs$solution, c(0, 0), label = "TikTak + L-BFGS")
+})
